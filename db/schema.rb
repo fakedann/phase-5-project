09cf93b63622e259b831e6b62595c5671e22b09b
Daniel Escalona <daniel07escalona@gmail.com>
2023-03-15 14:06:40 -0400

    Delete act

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_15_174615) do
+ActiveRecord::Schema.define(version: 2023_03_15_170547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,4 +83,5 @@ ActiveRecord::Schema.define(version: 2023_03_15_174615) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+
 end
